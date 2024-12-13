e6fea635f205abe0db230c58f2372e05141bde01
Brian Michel <bmoralescutting@gmail.com>
2024-12-13 11:36:19 -0500

    Primer programa

---
+++ S7CommunicationSsession.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using S7.Net;
+using S7ProfinetProtocol.S7ProfinetProtocol;
+
+namespace S7CommunicationSsession.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            S7ProfinetCommunicationSession session = new S7ProfinetCommunicationSession(CpuType.S71500,"127.0.0.1",0,0,new Guid());
+
+            session.Connect("");
+
+        }
+    }
+}
